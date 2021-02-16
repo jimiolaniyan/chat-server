ffbb9d4d91c6e6702b1fd49df1428dc751f7c87d
Olaniyan Folajimi <jimiolaniyan@gmail.com>
2021-02-16 20:45:52 +0100

    add main methods

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,3 +1,6 @@
 public class ChatClient {
+    public static void main(String[] args) {
+
+    }
 
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,3 +1,5 @@
 public class ChatServer {
+    public static void main(String[] args) {
 
+    }
 }
