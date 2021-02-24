7efd8e79c388ef41f2db5e8f15e58f8a0ac8e1c7
Olaniyan Folajimi <jimiolaniyan@gmail.com>
2021-02-24 11:05:14 +0100

    attempt unique uer

--- src/ServerEventImpl.java
+++ src/ServerEventImpl.java
@@ -16,15 +16,38 @@ public class ServerEventImpl implements ServerEvent {
     @Override
     public void login(ClientActions client) throws RemoteException {
         System.out.println("request");
+
+        // This is work in progress so not perfect yet
+        // if (findExistingClient(client) != null) {
+            // client.getMessage("Sorry username: " + client.getName() + " already exists. Please select another one");
+            // return;
+        // }
+
         //ClientActionsImpl cl = (ClientActionsImpl) client;
         clients.add(client);
         System.out.println("added");
 
-        if (messages.size() > 0)
+        if (messages.size() > 0) {
             client.getMessage("See all the messages you missed, " + client.getName() + "!");
             client.getMessages(this.messages);
+        }
 
         broadcast("[INFO] New client "+client.getName()+" has logged in");
+    }
+
+    private ClientActions findExistingClient(ClientActions client) {
+        return clients.stream()
+                .filter(c -> {
+                    boolean eq = false;
+                    try {
+                        eq = c.getName().equals(client.getName());
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    return eq;
+                })
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
@@ -42,18 +65,20 @@ public class ServerEventImpl implements ServerEvent {
 
     @Override
     public void broadcastExclude(String msg, ClientActions sender) throws RemoteException {
-        Message m = new Message(sender.getName(), msg);
-        messages.add(m);
+        if (msg != null && !msg.isEmpty()) {
+            Message m = new Message(sender.getName(), msg);
+            messages.add(m);
 
-        for(Enumeration<ClientActions> e = clients.elements(); e.hasMoreElements();) {
-            ClientActions c = (ClientActions) e.nextElement();
-            try {
-                if (!c.equals(sender)) {
-                    c.getMessage(sender.getName()+" - "+msg);
+            for (Enumeration<ClientActions> e = clients.elements(); e.hasMoreElements(); ) {
+                ClientActions c = (ClientActions) e.nextElement();
+                try {
+                    if (!c.equals(sender)) {
+                        c.getMessage(sender.getName() + " - " + msg);
+                    }
+                } catch (RemoteException rex) {
+                    System.out.println("Client is offline. Disconnected");
+                    clients.remove(c);
                 }
-            } catch (RemoteException rex) {
-                System.out.println("Client is offline. Disconnected");
-                clients.remove(c);
             }
         }
     }
